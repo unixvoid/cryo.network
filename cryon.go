90de7f19cdaf797b143787cf761bdd142dde648a
Matthew Faltys <mfaltys@gmail.com>
2016-08-21 20:24:23 -0500

    Add function to parse hostname and return proper ip if set

--- cryon.go
+++ cryon.go
@@ -11,10 +11,14 @@ import (
 	"github.com/unixvoid/glogger"
 )
 
+var (
+	domainName = "cryo.network"
+)
+
 func main() {
 	initLogger("debug")
 	// format the string to be :port
-	port := 53
+	port := 8053
 	fPort := fmt.Sprint(":", port)
 
 	udpServer := &dns.Server{Addr: fPort, Net: "udp"}
@@ -51,11 +55,12 @@ func route(w dns.ResponseWriter, req *dns.Msg) {
 func resolve(w dns.ResponseWriter, req *dns.Msg) {
 	hostname := req.Question[0].Name
 	glogger.Debug.Println(hostname)
+	domain := parseHostname(hostname)
 
 	// craft response
 	rr := new(dns.A)
 	rr.Hdr = dns.RR_Header{Name: hostname, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0}
-	addr := strings.TrimSuffix("127.0.0.1", "\n")
+	addr := strings.TrimSuffix(domain, "\n")
 	rr.A = net.ParseIP(addr)
 
 	// craft reply
@@ -66,4 +71,29 @@ func resolve(w dns.ResponseWriter, req *dns.Msg) {
 	// send it
 	w.WriteMsg(rep)
 	return
+}
+
+func parseHostname(hostname string) string {
+	// we expect at least domainName at the end. parse it out
+	if strings.Contains(hostname, domainName) {
+		parsedHostname := strings.Replace(hostname, fmt.Sprintf("%s.", domainName), "", -1)
+		glogger.Debug.Printf("parsedHostname: %s\n", parsedHostname)
+
+		// count the number of '.'s so we can only grab the ip from the end
+		//   this ip will end up being the last 4 '.'s
+
+		// make sure we have proper syntax. If less than 4 '.'s remaining, there is no ip
+		dotCount := strings.Count(parsedHostname, ".")
+		if dotCount < 4 {
+			glogger.Debug.Println("bad syntax, returning early")
+			return "127.0.0.1"
+		}
+		s := strings.Split(parsedHostname, ".")
+		ip := fmt.Sprintf("%s.%s.%s.%s", s[dotCount-4], s[dotCount-3], s[dotCount-2], s[dotCount-1])
+		glogger.Debug.Printf("Parsed ip: %s\n", ip)
+		return ip
+	} else {
+		// the domain was not in the query, return home
+		return "127.0.0.1"
+	}
 }
